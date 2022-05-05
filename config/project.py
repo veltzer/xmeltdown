73c4d59bb0884d2e29a0d52a58c4bb15ba628545
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-05 08:58:31 +0300

    fixes the build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "xmeltdown"
+project_long_description = "various utilities for X11"
+project_year_started = 2002
+project_description = """various utilities for X11"""
--- config/python.py
+++ config/python.py
@@ -1,2 +1,22 @@
-test_os = [ "ubuntu-20.04" ]
-test_python = [ "3.9" ]
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = []
+setup_requires = []
+run_requires = []
+test_requires = []
+dev_requires = [
+    "pymakehelper",
+    "pydmt",
+    "pyclassifiers",
+]
+
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+extras_require = {}
+
+python_requires = ">=3.9"
+test_os = ["ubuntu-20.04"]
+test_python = ["3.9"]
