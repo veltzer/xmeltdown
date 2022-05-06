22f52d0824d5c28c46fb0532e95d8ebfcab71565
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:13:16 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,20 +2,11 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-setup_requires = []
-run_requires = []
-test_requires = []
 dev_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
