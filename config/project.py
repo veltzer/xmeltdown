5a98925f9277e1142ea802c319c8019fa1886740
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:15 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,8 +1,5 @@
-import datetime
-import config.general
-
-project_name = "xmeltdown"
-project_long_description = """When I was back in the UNI I used to move people away from machine by running
+description_short = "utilities for X11"
+description_long = """When I was back in the UNI I used to move people away from machine by running
 xmeltdown on their machines. The user is happily working on his machine and
 suddenly the whole screen seems to melt. All you need to run this is X access
 to his screen (xhost + on his side). Some systems are configured (stupidly)
@@ -13,12 +10,3 @@ server in MIT and rescued them from obscurity. A lot of compilation fixes
 and some cleanups and bug fixes went into this and you can happily run
 xmeltdown again. Copyright of most of the stuff still belongs to the
 original authors. Free source never dies."""
-project_year_started = 2002
-project_description = "utilities for X11"
-
-project_copyright_years = ", ".join(
-    map(str, range(int(project_year_started), datetime.datetime.now().year + 1)))
-if str(config.general.general_current_year) == project_year_started:
-    project_copyright_years = config.general.general_current_year
-else:
-    project_copyright_years = f"{project_year_started} - {config.general.general_current_year}"
